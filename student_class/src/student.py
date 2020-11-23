a130ed7ab7c39e5c6c9422e4c33fdde38ad43e7f
Bela <belahamid@gmail.com>
2020-11-23 14:31:37 +0000

    initial commit

---
+++ student_class/src/student.py
@@ -0,0 +1,8 @@
+class Student:
+
+   def __init__(self, input_name, input_cohort):
+       self.name = input_name
+       self.cohort = input_cohort
+
+    def get_student_to_talk(self, student_name):
+        print("I can talk")
