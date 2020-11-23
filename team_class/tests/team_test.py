9a4b1f05dbc919be5f6102d2205d5a06d18d06ef
Bela <belahamid@gmail.com>
2020-11-23 16:08:39 +0000

    more tests passed

--- team_class/tests/team_test.py
+++ team_class/tests/team_test.py
@@ -14,11 +14,11 @@ class TestTeam(unittest.TestCase):
     def test_team_has_players(self):
         self.assertEqual(4, len(self.team.players))
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_team_has_coach(self):
         self.assertEqual("Irv Blitzer", self.team.coach)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_coach_can_be_changed(self):
         self.team.coach = "John Candy"
         self.assertEqual("John Candy", self.team.coach)
