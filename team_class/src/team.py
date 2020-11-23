7a3f9ddfff2366c41275508270ea06ef68dee3fc
Bela <belahamid@gmail.com>
2020-11-23 16:07:37 +0000

    second test passed

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -2,7 +2,7 @@ class Team:
 
     def __init__(self, input_name, input_players, input_coach):
         self.name = input_name
-        self.players = [input_players]
+        self.players = input_players
         self.coach = input_coach
         self.points = 0
     
