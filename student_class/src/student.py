e1253002fb86d6409459e4ec73ffe4ccacdc82b1
Bela <belahamid@gmail.com>
2020-11-23 14:51:30 +0000

    first test passed

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,8 +1,11 @@
 class Student:
 
-   def __init__(self, input_name, input_cohort):
+    def __init__(self, input_name, input_cohort):
        self.name = input_name
        self.cohort = input_cohort
 
     def get_student_to_talk(self, student_name):
         print("I can talk")
+
+    def say_favourite_language(self, language):
+        print("I love {language}")
